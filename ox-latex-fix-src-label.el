6908178c100131a049512e41038ec48c84bcd9d3
Holger Karl <hkarl@ieee.org>
2016-06-16 13:39:21 +0200

    this fixes some strange behavior: listing environment does not get created in minted

--- ox-latex-fix-src-label.el
+++ ox-latex-fix-src-label.el
@@ -9,7 +9,6 @@ and force and full as optional labels."
 	 (info (nth 1 args))
 	 (type (org-element-type datum))
 	 )
-    (print type)
     (case type
       (src-block
        (let ( (labelname (org-element-property :name datum))
@@ -23,6 +22,31 @@ and force and full as optional labels."
   )
 
 (advice-add 'org-latex--label :around #'org-latex-fix-src-code-label)
+
+
+;;; fix export for dot environments
+
+(defun org-latex-fix-src-block (oldsrcblockfct &rest args)
+  "Latex export mode has issues dealing with org files. This is an attempt to fix this."
+  (let* ((src-block (nth 0 args))
+	 (contents (nth 1 args))
+	 (info (nth 2 args))
+	 (lang (org-element-property :language src-block))
+	 )
+    (message "org-latex-fix-src-block")
+    (print lang)
+    (case lang
+      (dot
+       (message "dot block detected"))
+      (t
+       (format "\\begin{listing}[H]\n%s\n\\end{listing}"
+	       (apply oldsrcblockfct args)))
+      ) 
+    )
+  )
+
+(advice-add 'org-latex-src-block :around #'org-latex-fix-src-block)
+
 
 (provide 'ox-latex-fix-src-label)
 
