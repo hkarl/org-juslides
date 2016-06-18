6902f9812a8035e5e78015d65e5feedf41dd25b4
Holger Karl <hkarl@ieee.org>
2016-06-18 20:20:57 +0200

    concentrating on label fixing; fixing for src blocks and paragraphs

--- ox-latex-fix-src-label.el
+++ ox-latex-fix-src-label.el
@@ -1,16 +1,18 @@
 ;;; fix latex export: labels for source code blocks are not set correctly 
 
-(defun org-latex-fix-src-code-label (oldlabelfct &rest args)
+(defun org-latex-fix-label (oldlabelfct &rest args)
   "Fixing the incorrect generation of labels for source blocks. 
 Oringal function taks datum and info as compulsory, 
 and force and full as optional labels."
-  (message "before fix-minted")
   (let* ((datum (nth 0 args))
 	 (info (nth 1 args))
 	 (type (org-element-type datum))
 	 )
+    (print "fix label")
+    (print type)
+    (print (org-element-property :name datum))
     (case type
-      (src-block
+      ((src-block paragraph)
        (let ( (labelname (org-element-property :name datum))
 	     )
 	 (format "\\label{%s}" labelname)
@@ -21,31 +23,53 @@ and force and full as optional labels."
     )
   )
 
-(advice-add 'org-latex--label :around #'org-latex-fix-src-code-label)
+(advice-add 'org-latex--label :around #'org-latex-fix-label)
 
 
 ;;; fix export for dot environments
 
-(defun org-latex-fix-src-block (oldsrcblockfct &rest args)
-  "Latex export mode has issues dealing with org files. This is an attempt to fix this."
-  (let* ((src-block (nth 0 args))
-	 (contents (nth 1 args))
-	 (info (nth 2 args))
-	 (lang (org-element-property :language src-block))
-	 )
-    (message "org-latex-fix-src-block")
-    (print lang)
-    (case lang
-      (dot
-       (message "dot block detected"))
-      (t
-       (format "\\begin{listing}[H]\n%s\n\\end{listing}"
-	       (apply oldsrcblockfct args)))
-      ) 
-    )
-  )
+;; (defun org-latex-fix-src-block (oldsrcblockfct &rest args)
+;;   "Latex export mode has issues dealing with dot files. This is an attempt to fix this."
+;;   (let* ((src-block (nth 0 args))
+;; 	 (contents (nth 1 args))
+;; 	 (info (nth 2 args))
+;; 	 (lang (org-element-property :language src-block))
+;; 	 )
+;;     (message "new message org-latex-fix-src-block")
+;;     (print lang)
+;;     (print (type-of lang))
+;;     (if (equal lang "python")
+;; 	(format "\\begin{listing}[H]\n%s\n\\end{listing}"
+;; 		 (apply oldsrcblockfct args))
+;;       (apply oldsrcblockfct args))
 
-(advice-add 'org-latex-src-block :around #'org-latex-fix-src-block)
+;;     ;; (case lang
+;;     ;;   (("\"latex\"")
+;;     ;;    (print "latex detected")
+;;     ;;    )
+;;     ;;   (dot 
+;;     ;;    (format "\\begin{figure}\n%s\n\\end{figure}"
+;;     ;; 	       (apply oldsrcblockfct args))
+;;     ;;    )
+;;     ;;   (t
+;;     ;;    (format "\\begin{listing}[H]\n%s\n\\end{listing}"
+;;     ;; 	       (apply oldsrcblockfct args)))
+;;     ;;   ) 
+;;     )
+;;   )
+
+;; (advice-add 'org-latex-src-block :around #'org-latex-fix-src-block)
+
+
+;;; fix the latex export problem for dot files 
+
+;; (defun org-latex-dot-latex (link info)
+;;   (print "latex-dot-latex")
+;;   (print link)
+;;   (print info)
+;;   )
+
+;; (advice-add 'org-latex--inline-image :before #'org-latex-dot-latex)
 
 
 (provide 'ox-latex-fix-src-label)
